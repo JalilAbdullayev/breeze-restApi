8d16aa612910d84190c1367e0fe78cb038f55b28
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-28 17:03:25 +0400

    API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,27 +1,33 @@
 <?php
 
+use App\Http\Middleware\EnsureEmailIsVerified;
+use App\Models\Article;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
+        then: function() {
+            Route::bind('article', function($value) {
+                return Article::whereId($value)->orWhereSlug($value)->firstOrFail();
+            });
+        })->withMiddleware(function(Middleware $middleware) {
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            EnsureFrontendRequestsAreStateful::class,
         ]);
 
         $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'verified' => EnsureEmailIsVerified::class,
         ]);
 
         //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
+    })->withExceptions(function(Exceptions $exceptions) {
         //
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->prefix('v1')->group(function() {
+    Route::apiResource('articles', ArticleController::class);
+    Route::get('authors/{user}', [ArticleController::class, 'show']);
 });
