4740770af4d3ce9c2b6b395c029abaef4d4cc6da
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-28 11:13:08 +0400

    Create article model & migration

---
+++ app/Models/Article.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model {
+    public const TABLE = 'articles';
+    protected $table = self::TABLE;
+    protected $fillable = ['title', 'slug', 'body', 'author_id'];
+
+    public function id(): string {
+        return (string)$this->id;
+    }
+
+    public function title(): string {
+        return $this->title;
+    }
+
+    public function slug(): string {
+        return $this->slug;
+    }
+
+    public function body(): string {
+        return $this->body;
+    }
+}
