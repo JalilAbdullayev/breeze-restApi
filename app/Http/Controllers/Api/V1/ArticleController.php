1824bc7d8be1c426197b102d412768f7d64b70d9
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-13 16:59:23 +0400

    Test endpoints

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\ArticleCollection;
 use App\Http\Resources\V1\ArticleResource;
 use App\Models\Article;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
@@ -14,18 +17,23 @@ class ArticleController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index() {
+    public function index(): ArticleCollection {
         return new ArticleCollection(Article::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
-        $this->validate($request, [
-            'title' => ['required', 'max:20', 'unique:articles'],
+    public function store(Request $request): JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'max:20', 'unique:articles,title'],
             'body' => ['required', 'min:5'],
         ]);
+
+        if($validator->fails()) {
+            return Response::json($validator->errors(), 422);
+        }
+
         $article = Article::create([
             'title' => $request->input('title'),
             'slug' => Str::slug($request->input('title')),
@@ -38,18 +46,23 @@ class ArticleController extends Controller {
     /**
      * Display the specified resource.
      */
-    public function show(Article $article) {
+    public function show(Article $article): JsonResponse {
         return (new ArticleResource($article))->response()->setStatusCode(200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Article $article) {
-        $this->validate($request, [
-            'title' => ['required', 'max:20', Rule::unique('articles')->ignore($article->title())],
+    public function update(Request $request, Article $article): JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'title' => ['sometimes', 'max:20', Rule::unique('articles')->ignore($article->title(), 'title')],
             'body' => ['required', 'min:5'],
         ]);
+
+        if($validator->fails()) {
+            return Response::json($validator->errors(), 422);
+        }
+
         $article->update([
             'title' => $request->input('title'),
             'slug' => Str::slug($request->input('title')),
@@ -62,8 +75,8 @@ class ArticleController extends Controller {
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Article $article) {
+    public function destroy(Article $article): JsonResponse {
         $article->delete();
-        return response()->json(null, 204);
+        return Response::json(null, 204);
     }
 }
